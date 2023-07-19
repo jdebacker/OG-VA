38c8a3cf5ad0da7fc5381f82eb756c0d0a88b7e9
Richard Evans <rickecon@gmail.com>
2023-07-19 01:31:50 -0600

    Added pyproject.toml, pytest.ini, setup.py

---
+++ setup.py
@@ -0,0 +1,57 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    longdesc = fh.read()
+
+setuptools.setup(
+    name="ogva",
+    version="0.0.0",
+    author="Richard W. Evans and Jason DeBacker",
+    license="CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+    description="US state of Virginia calibration for OG-Core",
+    long_description_content_type="text/markdown",
+    long_description=longdesc,
+    url="https://github.com/TheCGO/OG-VA/",
+    download_url="https://github.com/TheCGO/OG-VA/",
+    project_urls={
+        "Issue Tracker": "https://github.com/TheCGO/OG-VA/issues",
+    },
+    packages=["ogva"],
+    package_data={"ogva": ["ogva_default_parameters.json", "data/PSID/*"]},
+    include_packages=True,
+    python_requires=">=3.7.7, <3.11",
+    install_requires=[
+        "numpy",
+        "psutil",
+        "scipy>=1.5.0",
+        "pandas>=1.2.5",
+        "matplotlib",
+        "dask>=2.30.0",
+        "distributed>=2.30.1",
+        "paramtools>=0.15.0",
+        "taxcalc>=3.0.0",
+        "requests",
+        "rpy2<=3.5.11",
+        "pandas-datareader",
+        "xlwt",
+        "openpyxl>=3.1.2",
+        "statsmodels",
+        "linearmodels",
+        "ogcore",
+    ],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: CC0 1.0 Universal public domain dedication",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    tests_require=["pytest"],
+)
